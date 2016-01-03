2ce46e66b707c001218af2be826913a0fdcffedc
Taru Karttunen <taruti@taruti.net>
2016-01-03 18:06:18 +0200

    Update main to use gtk3

--- main.go
+++ main.go
@@ -1,4 +1,22 @@
 package main
 
+import (
+	"log"
+
+	"github.com/gotk3/gotk3/gtk"
+)
+
 func main() {
+	gtk.Init(nil)
+
+	win, err := gtk.WindowNew(gtk.WINDOW_TOPLEVEL)
+	if err != nil {
+		log.Fatal("Unable to create window:", err)
+	}
+	win.Connect("destroy", func() {
+		gtk.MainQuit()
+	})
+
+	win.ShowAll()
+	gtk.Main()
 }
