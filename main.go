434b7465aa1214afe49b08ab8f16ef3ce7bc2e5c
Taru Karttunen <taruti@taruti.net>
2016-01-04 13:29:47 +0200

    Create TextView

--- main.go
+++ main.go
@@ -6,6 +6,27 @@ import (
 	"github.com/gotk3/gotk3/gtk"
 )
 
+func createMainWindow() (*gtk.Box, error) {
+	ttt, err := gtk.TextTagTableNew()
+	if err != nil {
+		return nil, err
+	}
+	tbuf, err := gtk.TextBufferNew(ttt)
+	if err != nil {
+		return nil, err
+	}
+	tv, err := gtk.TextViewNewWithBuffer(tbuf)
+	if err != nil {
+		return nil, err
+	}
+	vbox, err := gtk.BoxNew(gtk.ORIENTATION_VERTICAL, 2)
+	if err != nil {
+		return nil, err
+	}
+	vbox.PackStart(tv, true, true, 2)
+	return vbox, nil
+}
+
 func main() {
 	gtk.Init(nil)
 
@@ -16,6 +37,12 @@ func main() {
 	win.Connect("destroy", func() {
 		gtk.MainQuit()
 	})
+
+	wdg, err := createMainWindow()
+	if err != nil {
+		log.Fatal("Unable to create window:", err)
+	}
+	win.Add(wdg)
 
 	win.ShowAll()
 	gtk.Main()
